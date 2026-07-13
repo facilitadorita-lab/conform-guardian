c7ff5737259febe3d1b75d9b448e967e03fd1d4e
Mateus Amorim Santos <mateus.santos@itafria.com.br>
2026-07-12 23:14:57 -0300

    Corrige rota de detalhe de equipamentos

--- src/routes/equipamentos.tsx
+++ src/routes/equipamentos.tsx
@@ -1,7 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useRouterState } from "@tanstack/react-router";
 import { EquipamentosPage } from "@/pages/EquipamentosPage";
 
 export const Route = createFileRoute("/equipamentos")({
   head: () => ({ meta: [{ title: "Equipamentos - Conform Flow" }] }),
-  component: EquipamentosPage,
+  component: EquipamentosRoute,
 });
+
+function EquipamentosRoute() {
+  const pathname = useRouterState({ select: (state) => state.location.pathname });
+
+  if (pathname === "/equipamentos") {
+    return <EquipamentosPage />;
+  }
+
+  return <Outlet />;
+}
